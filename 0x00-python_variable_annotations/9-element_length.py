89b275b2358bec2f2d9c08078003433ad21e2166
Obioma29 <obiomachinwuko@yahoo.com>
2023-12-07 17:02:26 +0000

    Correction

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 return values with appropriate types."""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]
                    ) -> List[Tuple[Sequence, int]]:
     """Returns a list of tuples with the length of each element"""
