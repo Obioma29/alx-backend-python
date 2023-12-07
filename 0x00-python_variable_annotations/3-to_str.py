d90d73881933a8fe1cf1d2307423effd40a08ccb
Obioma29 <obiomachinwuko@yahoo.com>
2023-12-07 16:17:10 +0000

    Python variable annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains a function that returns a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of a float
+    """
+    return str(n)
